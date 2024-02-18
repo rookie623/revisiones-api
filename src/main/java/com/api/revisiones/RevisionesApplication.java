e21927809ecfc52880f6bd33e1e83728e2ba15f2
Matias Menoni <rookiemat@gmail.com>
2024-02-18 12:01:03 -0300

    first commit

---
+++ src/main/java/com/api/revisiones/RevisionesApplication.java
@@ -0,0 +1,13 @@
+package com.api.revisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/revisiones/RevisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.revisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
